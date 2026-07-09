c02600db8850549d6fb2ba331dd5d64932843b25
Nazrul Islam <nazrulislamtube@gmail.com>
2026-07-09 23:39:14 +0600

    home page duplicate component remove

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,19 +1,10 @@
-import HomeBestSeller from "@/components/home/best-sellers/best-seller";
 import { Hero } from "@/components/home/hero/hero";
-import SignaturePlates from "@/components/home/platter/signatur-plates";
-import PopularCategories from "@/components/home/popular-categories";
-import StoryChef from "@/components/our/our-chef";
-import StoryGallery from "@/components/our/story-gallery";
+
 
 export default function Home() {
   return(
     <>
       <Hero/>
-      <PopularCategories/>
-      <HomeBestSeller/>
-      <SignaturePlates/>
-     <StoryGallery/>
-     <StoryChef/>
     </>
   );
 }

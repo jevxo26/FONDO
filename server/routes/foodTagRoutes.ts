367fd3ac198c9c6837664c0617c6649f6f3b5d7f
jahidhasan-webdev01 <jahidhasan.webdev01@gmail.com>
2026-07-10 00:03:22 +0600

    create vendor schema and a vendor to db

--- server/routes/foodTagRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { TagController } from '../controllers/foodTagController';
-
-const router = Router();
-
-router.get('/', TagController.getAll);
-
-export default router;
